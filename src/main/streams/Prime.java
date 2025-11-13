4b46a860e6f979de1c34810b2e1ffc0aee5e1fbd
isaquliyev <isa.quluyev.wip@gmail.com>
2025-11-13 10:14:23 +0100

    - stream version of Prime static methods

--- src/main/streams/Prime.java
+++ src/main/streams/Prime.java
@@ -1,22 +1,33 @@
 package streams;
 
+import java.util.stream.IntStream;
+
 public class Prime {
 
+
+    /**
+     * The function that checks if number is prime
+     * @param n The provided number
+     * @return Boolean value. If number is prime then true, false otherwise
+     */
     static public boolean isPrime(int n) {
         if (n <= 1) return false;
         if (n <= 3) return true;
         if (n % 2 == 0 || n % 3 == 0) return false;
 
-        for (int i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0) return false;
-        }
-        return true;
+
+        return IntStream.iterate(5, i -> i * i <= n, i -> i + 6)
+                .noneMatch(i -> n % i == 0 || n % (i + 2) == 0);
+
     }
 
+    /**
+     * The function that gets next prime number
+     * @param n Initial integer
+     * @return Next prime number of given integer
+     */
     static public int nextPrime(int n) {
-        if (n < 2) return 2;
-        while (true) {
-            if (isPrime(++n)) return n;
-        }
+        return IntStream.iterate(n + 1, i -> i + 1)
+                .filter(Prime::isPrime).findFirst().orElseThrow();
     }
 }
