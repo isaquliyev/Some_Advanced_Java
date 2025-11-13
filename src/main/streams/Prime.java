a4b91abb3ce9bb65e2ca0157e990725cfba43c56
isaquliyev <isa.quluyev.wip@gmail.com>
2025-11-13 09:57:13 +0100

    - some prime related stream problems

---
+++ src/main/streams/Prime.java
@@ -0,0 +1,22 @@
+package streams;
+
+public class Prime {
+
+    static public boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+
+    static public int nextPrime(int n) {
+        if (n < 2) return 2;
+        while (true) {
+            if (isPrime(++n)) return n;
+        }
+    }
+}
--- src/main/streams/StreamsSandbox.java
+++ src/main/streams/StreamsSandbox.java
@@ -1,67 +1,18 @@
 package streams;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamsSandbox {
-    public static void main(String[] strings) {
-        /*
-        int len = strings.length;
-        for(int i = 0; i < len; i++) {
-            System.out.println(strings[len - i - 1].length());
-        }
-        */
+    public static void main(String[] args) {
 
-        /*
-        IntStream.range(0, strings.length)
-                .mapToObj(i -> strings[strings.length - i - 1])
-                .map(String::length)
-                .forEach(System.out::println);
+        /// IntStream.range(0, args.length).mapToObj(i -> args[args.length - 1 - i]).forEach(System.out::println);
 
-         */
+        /// System.out.println(
+        /// Arrays.stream(args).filter(s -> s.matches("[0-9]+")).mapToInt(Integer::parseInt).filter(x -> x > 8 && x % 2 == 0).sum());
 
-        int len = strings.length;
-        int sum = Arrays.stream(strings).map(StreamsSandbox::parseInt)
-                .filter(integer -> integer >= 8 && integer % 2 == 0)
-                .mapToInt(Integer::intValue)
-                .sum();
-
-        System.out.println(sum);
-
-
-    }
-
-
-    public static int parseInt(String str) {
-        try {
-            return Integer.parseInt(str);
-        } catch (NumberFormatException e) {
-            return 0;
-        }
-    }
-
-    public static List<Integer> primeReturner(int N) {
-        int count = 0;
-        int num = 2;
-        List<Integer> primeNumbers = new ArrayList<>();
-
-        while (count != N) {
-            boolean prime = true;
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) {
-                    prime = false;
-                    break;
-                }
-            }
-            if (prime) {
-                count++;
-                primeNumbers.add(num);
-            }
-            num++;
-        }
-
-        return primeNumbers;
+        Stream.iterate(2, Prime::nextPrime).limit(8).forEach(System.out::println);
     }
 }
